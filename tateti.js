0f0cc7924387f735380da967f823a336d1566ade
ara-rojas <araceliro.guanes@gmail.com>
2023-05-12 10:31:09 -0400

    tateti juego

---
+++ tateti.js
@@ -0,0 +1,8 @@
+var jugador1 = document.getElementById(jugador1);
+
+var jugador2 = document.getElementById(jugador2);
+var jugar = document.getElementById(btn);
+function click(jugador1, jugador2) {
+  if (btn) prompt("Los jugadores son ", jugador1, "y", jugador2);
+}
+console.log(click(jugador1, jugador2));
